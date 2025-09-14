98ae51736153abc5e41e535a93e6e9dd69af6d88
‮sgniD ot duP <dhyslexia@gmail.com>
2025-09-14 14:30:20 +0700

    nambah fitur a

--- odd_even.py
+++ odd_even.py
@@ -5,4 +5,9 @@ print(22 % 3) # hasilnya 1 (sisa bagi 1)
 print(65 % 3) # hasilnya 2 (sisa bagi 2)
 
 
+input_user = int(input("Masukkan angka: "))
 
+if input_user % 2 == 0:
+    print(f"{input_user} adalah bilangan genap.")
+else:
+    print(f"{input_user} adalah bilangan ganjil.")
